1f12177916c04d7b6a97940bd3484469ff410f7d
Alvaro <alvaro.a.cruz.j@gmail.com>
2024-07-09 18:39:53 -0600

    carrusel partial functional

---
+++ script/scripts.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    initialSlide: 2,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+});
