7ec6a6c7be00406d8da593696961e5080a393628
Alvaro <alvaro.a.cruz.j@gmail.com>
2024-07-09 19:37:41 -0600

    carrusel functional corrected

--- script/scripts.js
+++ script/scripts.js
@@ -1,10 +1,11 @@
 const swiper = new Swiper('.swiper-container', {
-    loop: true,
-    slidesPerView: 3,
-    spaceBetween: 40,
-    initialSlide: 2,
-    autoplay: {
-        delay: 2000,
-        disableOnInteraction: false,
+    loop: false,              // Para que el swiper haga loop
+    slidesPerView: 3,        // Número de slides visibles por vista
+    spaceBetween: 40,        // Espacio entre slides
+    initialSlide: 0,         // Slide inicial (segunda tarjeta)
+    autoplay: {              // Configuración del autoplay
+        delay: 2000,         // Intervalo de tiempo en milisegundos entre slides
+        disableOnInteraction: false,  // No desactivar el autoplay al interactuar manualmente
     },
+    loopAdditionalSlides: 0, // Asegura que se vea la slide adicional al inicio y al final en el loop
 });
