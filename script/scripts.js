051efb3feca2e8f509efafe2f08bb1df27f3aab7
Alvaro <alvaro.a.cruz.j@gmail.com>
2024-07-10 00:04:37 -0600

    procedural glasses and fixed color

--- script/scripts.js
+++ script/scripts.js
@@ -7,5 +7,5 @@ const swiper = new Swiper('.swiper-container', {
         delay: 2000,         // Intervalo de tiempo en milisegundos entre slides
         disableOnInteraction: false,  // No desactivar el autoplay al interactuar manualmente
     },
-    loopAdditionalSlides: 0, // Asegura que se vea la slide adicional al inicio y al final en el loop
+    loopAdditionalSlides: 0,
 });
