7e9b68aef3badd16eada8749fab61b615df7e852
Alvaro <alvaro.a.cruz.j@gmail.com>
2024-07-13 00:03:10 -0600

    first sidebar

---
+++ script/scripts_mainPageAccess.js
@@ -0,0 +1,32 @@
+function openNav() {
+document.getElementById("mySidebar").style.width = "250px";
+}
+
+function closeNav() {
+document.getElementById("mySidebar").style.width = "0";
+}
+
+
+
+
+
+
+const swiper = new Swiper('.swiper-container', {
+    loop: false,              // Para que el swiper haga loop
+    slidesPerView: 3,        // Número de slides visibles por vista
+    spaceBetween: 40,        // Espacio entre slides
+    initialSlide: 0,         // Slide inicial (segunda tarjeta)
+    autoplay: {              // Configuración del autoplay
+        delay: 2000,         // Intervalo de tiempo en milisegundos entre slides
+        disableOnInteraction: false,  // No desactivar el autoplay al interactuar manualmente
+    },
+    loopAdditionalSlides: 0,
+});
+
+document.querySelector('.explore').addEventListener('mouseover', () => {
+    document.querySelector('.arrow-button-logo').classList.add('animate');
+});
+
+document.querySelector('.explore').addEventListener('mouseout', () => {
+    document.querySelector('.arrow-button-logo').classList.remove('animate');
+});
